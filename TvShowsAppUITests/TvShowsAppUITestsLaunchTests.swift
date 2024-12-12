e8c8a1a1ddf69e9a4f16cdbc18f1b84b980c30ef
Bruno Morgan <bumorgan@hotmail.com>
2024-12-12 08:41:24 -0300

    Initial Commit

---
+++ TvShowsAppUITests/TvShowsAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TvShowsAppUITestsLaunchTests.swift
+//  TvShowsAppUITests
+//
+//  Created by Bruno de Mello Morgan on 12/12/24.
+//
+
+import XCTest
+
+final class TvShowsAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
